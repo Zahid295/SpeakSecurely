54ebe054bad21049c9aba2373ed699d62e8100ac
Zahid295 <zahid@295>
2024-06-13 00:18:05 +0100

    Fix connection error

--- speaksecurely-backend/app.py
+++ speaksecurely-backend/app.py
@@ -1,29 +1,13 @@
 from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-from flask import Flask
 from flask_pymongo import PyMongo
-from dotenv import load_dotenv
+from env import Config
 import os
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+mongo = PyMongo(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-load_dotenv()
-
-app.config["MONGO_URI"] = os.getenv("DATABASE_URL")
-
-mongo = PyMongo(app)
