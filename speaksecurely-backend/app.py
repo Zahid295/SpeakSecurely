65ec520b78a3e19f48137e887deb837ab4c9e793
Malekulashter-K <157482554+Malekulashter-K@users.noreply.github.com>
2024-06-12 17:39:28 +0000

    setup DB

---
+++ speaksecurely-backend/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+load_dotenv()
+
+app.config["MONGO_URI"] = os.getenv("DATABASE_URL")
+
+mongo = PyMongo(app)
